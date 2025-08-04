ecffdf19ec360ae0783c5c99192be2d35c581a6e
Anuradha <ishiwaraanuradha@gmail.com>
2025-08-04 18:36:16 +0530

    --fix layout issue

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,8 @@ import "./globals.css";
 
 // components
 import ThemeProvider from "@/components/ThemeProvider";
-
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 // Load Poppins font
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -26,9 +27,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.variable} scroll-smooth dark`}>
       <body className="font-poppins antialiased leading-8 overflow-x-hidden min-h-screen transition-colors duration-500 bg-light-base text-light-text dark:bg-dark-base dark:text-dark-text">
+        <Header />
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
         </ThemeProvider>
+        <Footer />
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,7 @@
 //components
 import About from "@/components/About";
 import Contactme from "@/components/Contactme";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+
 import Hero from "@/components/Hero";
 import Review from "@/components/Review";
 import Services from "@/components/Services";
@@ -10,14 +9,12 @@ import Work from "@/components/Work";
 export default function HomePage() {
   return (
     <main className="min-h-screen">
-      <Header />
       <Hero />
       <About />
       <Services />
       <Work />
       <Review />
       <Contactme />
-      <Footer />
     </main>
   );
 }
