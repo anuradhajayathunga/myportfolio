3a240759d1a281158840e5cd750939b4160d33cc
Anuradha <ishiwaraanuradha@gmail.com>
2025-08-07 23:58:32 +0530

    --fix positions data file loading issue

--- components/Review.jsx
+++ components/Review.jsx
@@ -32,7 +32,49 @@ import {
 import { BsChatQuoteFill } from "react-icons/bs";
 import { useIsMobile } from "../hooks/useIsMobile";
 const prefix = process.env.NODE_ENV === "production" ? "/myportfolio" : "";
-import { positions } from "../lib/positions"; 
+
+ const positions = [
+  {
+    position: { base: "top-24 left-2 ", md: "top-32 left-8" },
+    size: { base: 72, md: 80 },
+    scale: 1.0,
+    delay: 0,
+  },
+  {
+    position: { base: "top-8 right-12", md: "top-12 right-12" },
+    size: { base: 48, md: 60 },
+    scale: 0.95,
+    delay: 0.1,
+  },
+  {
+    position: { base: "top-4 left-8", md: "top-4 left-16 -z-20" },
+    size: { base: 40, md: 56 },
+    scale: 0.85,
+    delay: 0.2,
+  },
+  {
+    position: { base: "top-36 right-4", md: "top-48 right-8" },
+    size: { base: 56, md: 72 },
+    scale: 0.9,
+    delay: 0.3,
+  },
+  {
+    position: {
+      base: "-top-10 right-1/2 -z-20",
+      md: "-top-10 left-1/2 -translate-x-1/2 -z-20",
+    },
+    size: { base: 56, md: 72 },
+    scale: 0.95,
+    delay: 0.4,
+  },
+  {
+    position: { base: "top-64 right-13", md: "top-72 right-24 -z-20" },
+    size: { base: 36, md: 48 },
+    scale: 0.8,
+    delay: 0.5,
+  },
+];
+
 const reviewData = [
   {
     avatar: `${prefix}/reviews/avatar-5.png`,
